347c4ea471bef872ce49a5d02e20a5097af24bf4
Jia-Rung Chang <jrchang@sslab.cs.nctu.edu.tw>
2017-10-04 19:35:42 +0800

    [Thesis] Automatic random pass set Executer

--- DSOAO/random_select/Driver.py
+++ DSOAO/random_select/Driver.py
@@ -1,16 +1,62 @@
 #!/usr/bin/python3
 import RandomGenerator as RG
+import shlex
+import subprocess as sp
 import os
+import time
+from time import gmtime, strftime
 
 class Executer:
+    # numbers of random pass set execution
+    repeat = 50
+    #path is relative to "llvm_source/DSOAO/random_select"
+    # ["directory path", "build command", [available benchmarks commands], "clean command"]
+    benchmark_build_run_list = [
+        #["benchmark/helloworld" ,"clang -O1 -o hello hello.c", ["./hello", "ls"], "rm hello"],
+        ["benchmark/botan" ,"make -j8", ["./botan-test", ], "make clean"],
+    ]
     def run(self):
-        for i in range(10):
+        localtime = time.strftime("%m%d-%H:%M", time.localtime())
+        prev_cwd = os.getcwd()
+        Result_FileLoc = prev_cwd + "/results_time/result_" + localtime
+        Result_File = open(Result_FileLoc, "w")
+        Result_File.close()
+        for i in range(self.repeat):
+            print("Iteration Start-----------------------------------------")
             #generate pass set
             rg_driver = RG.Driver()
-            rg_driver.run()
-            #run command
-            print("---%d---" % i)
-            os.system("clang -O1 -o benchmark/hello benchmark/hello.c")
+            #mean should between 0~1
+            mean = (i + 1) / (self.repeat + 1)
+            rg_driver.run(mean)
+
+            #build benchmarks for this pass set
+            for build_bench in self.benchmark_build_run_list:
+                cwd = prev_cwd + "/" + build_bench[0]
+                os.chdir(cwd)
+                print("Build Start*******************************************")
+                print("clean=\"{}\"".format(build_bench[3]))
+                p = sp.Popen(shlex.split(build_bench[3]))
+                p.wait()
+                print("build=\"{}\"".format(build_bench[1]))
+                p = sp.Popen(shlex.split(build_bench[1]))
+                p.wait()
+                print("Build End*******************************************")
+                #Run built benchmarks
+                for run_single in build_bench[2]:
+                    #run command
+                    print("<<<<<<  RUN=\"{}\"  >>>>>>".format(run_single))
+                    start_time = time.perf_counter()
+                    p = sp.Popen(shlex.split(run_single))
+                    p.wait()
+                    end_time = time.perf_counter()
+                    period = end_time - start_time
+                    Result_File = open(Result_FileLoc, "a")
+                    Result_File.write("{}\n".format(period))
+                    Result_File.close()
+                os.chdir(prev_cwd)
+            print("Iteration End-----------------------------------------")
+        os.chdir(prev_cwd)
+        print("Done. Results written to \"{}\"".format(Result_FileLoc))
 
 
 
--- DSOAO/random_select/RandomGenerator.py
+++ DSOAO/random_select/RandomGenerator.py
@@ -7,43 +7,42 @@ class Candidate:
         return list(range(1, 34+1))
 
 class RandomPick:
-    def run(self, list):
+    def run(self, list, mean):
         res_list = []
         res_list.append(list[0])
         for num in list[1:]:
-            if random.uniform(0, 1) > 0.5:
+            if random.uniform(0, 1) < mean:
                 res_list.append(num)
         return res_list
 
 class Driver:
-    def run(self):
+    def run(self, mean):
         cand = Candidate()
         #l is the ordered candidates
         l = cand.generator() 
         cryptorand = SystemRandom()
         #shuffle it to create randomness
-        for i in range(10):
-            cryptorand.shuffle(l)
-            rand = RandomPick() 
-            #pick at least one as candidate
-            target_list = rand.run(l)
-            #shuffle to keep first does not always the same
-            cryptorand.shuffle(target_list)
-            """
-            print(target_list)
-            """
-            #open file with buffering, text is not able to get rid of buffering
-            target_file = open("/home/jrchang/workspace/llvm/DSOAO/random_select/InputSet", "w")
-            for ps in target_list:
-                target_file.write("%s " % ps)
-            target_file.write("\n")
-            target_file.close()
-            """
-            target_file = open("/home/jrchang/workspace/llvm/experiment/random_select/InputSet", "r")
-            print(target_file.read())
-            target_file.close()
-            """
+        cryptorand.shuffle(l)
+        rand = RandomPick() 
+        #pick at least one as candidate
+        target_list = rand.run(l, mean)
+        #shuffle to keep first does not always the same
+        cryptorand.shuffle(target_list)
+        """
+        print(target_list)
+        """
+        #open file with buffering, text is not able to get rid of buffering
+        file_loc = "/home/jrchang/workspace/llvm/DSOAO/random_select/InputSet"
+        target_file = open(file_loc, "w")
+        for ps in target_list:
+            target_file.write("%s " % ps)
+        target_file.write("\n")
+        target_file.close()
+        target_file = open(file_loc, "r")
+        print(target_file.read())
+        target_file.close()
 
 if __name__ == '__main__':
     drv = Driver()
-    drv.run()
+    mean = 0.05
+    drv.run(mean)
