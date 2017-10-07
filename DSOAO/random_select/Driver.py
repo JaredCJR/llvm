ceb86895f79d44d45fe2f8bf7e5cdbfb6d0a644f
Jia-Rung Chang <jrchang@sslab.cs.nctu.edu.tw>
2017-10-07 15:16:16 +0800

    Add notification

--- DSOAO/random_select/Driver.py
+++ DSOAO/random_select/Driver.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import RandomDriver as RD
-
+import time
 
 if __name__ == '__main__':
     round = 5
+    print("Remember to set 'clang' and 'clang++' to customized build.")
+    print("It will start after 5 secs...")
+    time.sleep(5)
     for i in range(round):
         execute = RD.Executer()
         execute.run()
