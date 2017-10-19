d96484ed724e207aae89222e2e2390b19f288fab
Jia-Rung Chang <jrchang@sslab.cs.nctu.edu.tw>
2017-10-20 01:20:07 +0800

    [Thesis] Solve return value issue.
    TODO: failed cases

--- DSOAO/random_select/LLVMTestSuiteScript/LitMimic.py
+++ DSOAO/random_select/LLVMTestSuiteScript/LitMimic.py
@@ -26,8 +26,10 @@ class LitMimic:
     BuiltPath = None
     TargetDirLists = None
     #Make sure that the elf already exists
-    PyActorLoc = "./PyActor/MimicAndFeatureExtractor.py"
-    PyCallerLoc = "./PyActor/PyCaller"
+    PyActorLoc_withStdin = "./PyActor/WithStdin/MimicAndFeatureExtractor.py"
+    PyActorLoc_withoutStdin = "./PyActor/WithoutStdin/MimicAndFeatureExtractor.py"
+    PyCallerLoc_withStdin = "./PyActor/WithStdin/PyCaller"
+    PyCallerLoc_withoutStdin = "./PyActor/WithoutStdin/PyCaller"
 
     def __init__(self):
         target = TargetBenchmarks()
@@ -47,21 +49,37 @@ class LitMimic:
                 for file in files:
                     test_pattern = '.test'
                     if file.endswith(test_pattern):
+                        #Does this benchmark need stdin?
+                        NeedStdin = False
+                        TestFilePath = os.path.join(os.path.abspath(root), file)
+                        with open(TestFilePath, "r") as TestFile:
+                            for line in TestFile:
+                                if line.startswith("RUN:"):
+                                    if line.find(" < ") != -1:
+                                        NeedStdin = True
                         #Do what we want: rename elf and copy actor
                         ElfName = file.replace(test_pattern, '')
                         ElfPath = os.path.join(root, ElfName)
                         NewElfName = ElfName + ".OriElf"
                         NewElfPath = os.path.join(root, NewElfName)
+                        #based on "stdin" for to copy the right ones
+                        if NeedStdin == True:
+                            PyCallerLoc = self.PyCallerLoc_withStdin
+                            PyActorLoc = self.PyActorLoc_withStdin
+                        else:
+                            PyCallerLoc = self.PyCallerLoc_withoutStdin
+                            PyActorLoc = self.PyActorLoc_withoutStdin
+                        #copy it
                         if os.path.exists(ElfPath) == True:
                             #rename the real elf
                             shutil.move(ElfPath, NewElfPath)
                             #copy the feature-extractor
-                            shutil.copy2(self.PyActorLoc, ElfPath + ".py")
-                            #copy to PyCaller to feature-extractor
-                            if os.path.exists(self.PyCallerLoc) == True:
-                                shutil.copy2(self.PyCallerLoc, ElfPath)
+                            shutil.copy2(PyActorLoc, ElfPath + ".py")
+                            #copy the PyCaller
+                            if os.path.exists(PyCallerLoc) == True:
+                                shutil.copy2(PyCallerLoc, ElfPath)
                             else:
-                                log.err("Please \"$ make\" to get PyCaller in {}\n".format(self.PyCallerLoc))
+                                log.err("Please \"$ make\" to get PyCaller in {}\n".format(PyCallerLoc))
                                 return
                         else:
                             log.err("This elf={} filed to build?\n".format(ElfPath))
