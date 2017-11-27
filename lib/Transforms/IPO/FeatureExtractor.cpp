ff0eb5ffc1e87c852a252cc152d407019eeb54bd
JaredCJR <jaredcjr.tw@gmail.com>
2017-11-27 22:11:35 +0800

    Rename the PassPredictionPass to FeatureExtractorPass

--- lib/Transforms/IPO/FeatureExtractor.cpp
+++ lib/Transforms/IPO/FeatureExtractor.cpp
@@ -2,47 +2,47 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Transforms/IPO/PassPrediction.h"
+#include "llvm/Transforms/IPO/FeatureExtractor.h"
 #include "llvm/Transforms/IPO.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/IR/LegacyPassManagers.h"
 using namespace llvm;
 
-#define DEBUG_TYPE "PassPrediction(DEBUG_TYPE)"
+#define DEBUG_TYPE "FeatureExtractor(DEBUG_TYPE)"
 
-STATISTIC(PassPredictionCounter, "Counts number of functions greeted");
+STATISTIC(FeatureExtractorCounter, "Counts number of functions greeted");
 
 namespace {
-  struct PassPrediction : public FunctionPass {
+  struct FeatureExtractor : public FunctionPass {
     static char ID; // Pass identification, replacement for typeid
-    PassPrediction() : FunctionPass(ID) {}
+    FeatureExtractor() : FunctionPass(ID) {}
 
     bool runOnFunction(Function &F) override;
   };
 }
 
-char PassPrediction::ID = 0;
+char FeatureExtractor::ID = 0;
 
 /// (passName, arg, name, cfg, analysis)
-INITIALIZE_PASS_BEGIN(PassPrediction, "passpredict",
+INITIALIZE_PASS_BEGIN(FeatureExtractor, "Feature-Extractor",
                 "PassPrediction Pass", false, false)
 //INITIALIZE_PASS_DEPENDENCY(TargetLibraryInfoWrapperPass)
-INITIALIZE_PASS_END(PassPrediction, "passpredict",
+INITIALIZE_PASS_END(FeatureExtractor, "Feature-Extractor",
                 "PassPrediction Pass", false, false)
 
-FunctionPass *llvm::createPassPredictionPass() {
-  return new PassPrediction();
+FunctionPass *llvm::createFeatureExtractorPass() {
+  return new FeatureExtractor();
 }
 
-bool PassPrediction::runOnFunction(Function &F) {
-  ++PassPredictionCounter;
-  errs() << "PassPrediction: ";
+bool FeatureExtractor::runOnFunction(Function &F) {
+  ++FeatureExtractorCounter;
+  errs() << "FeatureExtractorPass: ";
   errs().write_escaped(F.getName()) << '\n';
 
-  ///try add pass
+  ///try add analysis pass ?
   //PMTopLevelManager PM;
   //PM.schedulePass(createDeadCodeEliminationPass());
   return false;
 }
 
-static RegisterPass<PassPrediction> X("PassPrediction", "PassPrediction Pass");
+static RegisterPass<FeatureExtractor> X("FeatureExtractor", "FeatureExtractor Pass");
